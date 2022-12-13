4fe21f34da7e09f7c978b9e3e49bff1b8607c3ed
aryanmahendru <114501779+aryanmahendru@users.noreply.github.com>
2022-12-13 09:33:30 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿Characters spyderman=new Characters("Spyderman","Anyone can wear the mask","I believe there’s a hero in all of us.");
-Characters kungfupanda=new Characters("Kung Fu Panda","There is no secret ingredient","Oogway There are no accidents");
-Characters.Speak(spyderman,1);
-Characters.SetLevel(spyderman,2);
-Characters.Speak(kungfupanda,2);
+﻿Characters spyderman=new Characters("Spyderman","Anyone can wear the mask","I believe there’s a hero in all of us.");  //Character1
+Characters kungfupanda=new Characters("Kung Fu Panda","There is no secret ingredient","Oogway There are no accidents"); //Character2
+Characters.Speak(spyderman,1);  //making character1 speaking phrase1
+Characters.SetLevel(spyderman,2);  //setting level of character1 to 2
+Characters.Speak(kungfupanda,2);  //making character2 speaking phrase2
 
 class Characters{
     public string Name{get;set;}
@@ -16,19 +16,19 @@ class Characters{
         this.Phrase2=phrase2;
         this.Level=0;
     }
-    public static void Speak(Characters input,int phrasenum){
+    public static void Speak(Characters input,int phrasenum){  //my function for speaking character phrase 1 or 2
         if(phrasenum == 1){
             Console.WriteLine(input.Phrase1);
         }
         else if(phrasenum == 2){
             Console.WriteLine(input.Phrase2);
         }
-        else{
+        else{   //giving error if the input is not 1 or 2
             Console.WriteLine("The input should be 1 or 2");
         }
     }
 
-    public static void SetLevel(Characters input1,int level){
+    public static void SetLevel(Characters input1,int level){  //my function for setting level of Characters
         input1.Level=level;
         Console.WriteLine(input1.Name + " has now level" + level);
     }
